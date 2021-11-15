9a3e9a1d3e95e08e990523ccc7881c84557bc69f
Frank Elsinga <frank@elsinga.de>
2021-11-15 16:33:24 +0100

    a few minor project setup fixes

--- common/views.py
+++ common/views.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.decorators import login_required
+from collections import Callable
+
+from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from common.utils.typing import AuthWSGIRequest
+
+klopapier_staff_member_required: Callable = staff_member_required(login_url="login")
 
 
 def leaderboard(request: AuthWSGIRequest) -> HttpResponse:
