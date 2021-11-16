eef28024d293e29a2e0aabc70fe3dd21eab0b6dd
Frank Elsinga <frank@elsinga.de>
2021-11-16 22:28:29 +0100

    fixed the fixture for generating the mocking data

--- common/fixture.py
+++ common/fixture.py
@@ -1,4 +1,5 @@
 import random
+import string
 from subprocess import run  # nosec: used for flushing the db
 
 import django.utils.timezone
@@ -9,6 +10,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.utils.datetime_safe import datetime
 
+import challenges.models as m_challenges
 import common.models as m_common
 
 
@@ -26,6 +28,64 @@ def showroom_fixture_state_no_confirmation():
     _generate_superusers()
 
     # app common
+    _generate_ideas()
+
+    # app challenges
+    _generate_challenges()
+
+
+def _generate_ideas():
+    for _ in range(random.randint(15, 30)):
+        content = random.choice(
+            [
+                f"{rand_company_name()} for president",
+                f"Could you kindly feature {rand_company_name()} on your next issue? "
+                f"Kindly, {rand_firstname()} {rand_last_name()}",
+                f"{rand_firstname()} enjoyed this",
+                "https://fsmpi.de/aushaenge",
+                "https://fsmpi.de/faq",
+            ]
+            + [f"https://xkcd.com/{random.randint(1, 2542)}/"] * 3,
+        )
+        if not m_common.Idea.objects.filter(content=content).exists():
+            m_common.Idea.objects.create(
+                votes=random.choice((random.randint(1, 150), 69, random.randint(1, 10), 0, 0)),
+                content=content,
+            )
+
+
+def _generate_challenges():
+    challenges = []
+    for _ in range(random.randint(5, 15)):
+        url = random.choice(("https://fsmpi.de", "https://fsmpi.de/aushaenge", "https://fsmpi.de/units"))
+        key = rand_firstname() + "_" + rand_company_name()
+        challenges.append(
+            m_challenges.Challenge.objects.create(
+                key=key,
+                prompt=lorem.sentence(),
+                description=lorem.paragraph(),
+                redirect_action=url,
+            ),
+        )
+    for challenge in challenges:
+        shortlinks = random.choice([0] * 2 + [1] * 3 + [3] * 4 + [6])
+        for _ in range(shortlinks):
+            short_link = "".join([random.choice(string.ascii_lowercase)] * random.randint(5, 10))
+
+            if not m_challenges.ChallengeShortLink.objects.filter(short_link=short_link).exists():
+                m_challenges.ChallengeShortLink.objects.create(
+                    challenge=challenge,
+                    short_link=short_link,
+                )
+    for challenge in challenges:
+        secrets = random.choice([0] + [1] * 3 + [3] * 4 + [6])
+        for _ in range(secrets):
+            secret=rand_company_name().lower()
+            if not m_challenges.ChallengeSecret.objects.filter(challenge=challenge,secret=secret).exists():
+                m_challenges.ChallengeSecret.objects.create(
+                    challenge=challenge,
+                    secret=secret,
+                )
 
 
 def _generate_superusers():
