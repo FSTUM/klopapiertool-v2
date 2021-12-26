e822b993104ccb078f4bb40fcc535f79f64ea4e1
Frank Elsinga <frank@elsinga.de>
2021-12-26 01:41:11 +0100

    migrated django-compref-keycloak to v1.2 to enable the logout-button

--- klopapier/urls.py
+++ klopapier/urls.py
@@ -27,7 +27,7 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # 
 if settings.USE_KEYCLOAK:
     urlpatterns += [
         # Auth
-        path("logout/", LogoutView.as_view(), name="logout"),
+        path("logout/", RedirectView.as_view(pattern_name="oidc_logout"), name="logout"),
         path("oidc/", include("mozilla_django_oidc.urls")),
         path("login/failed/", common.views.login_failed),
     ]
